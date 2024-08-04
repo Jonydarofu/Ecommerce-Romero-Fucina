27ca2af40d104c0fb87490126aeb30f2dbc89ac5
jonathan romero fucina <jonathan07_1@hotmail.com>
2024-08-04 13:23:02 -0300

    nuevo repositorio

--- Ecommerce Romero Fucina/src/App.jsx
+++ Ecommerce Romero Fucina/src/App.jsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+import Footer from "./componentes/Footer/Footer";
+import CartWidget from "./componentes/CartWidget/CartWidget";
+import Navbar from "./componentes/Navbar/navbar";
+import ProductCard from "./componentes/ProductCard/ProductCard";
+import ItemListContainer from "./componentes/ItemListContainer/ItemListContainer";
+import ItemCount from "./componentes/ItemCount/ItemCount";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ConsumeindoApis from "./componentes/ConsumiendoApis/ConsumiendoApis";
+import { BsTypeH1 } from "react-icons/bs";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const onAdd = (cantidad) => {
+    console.log(`se agrrego al carrito ${cantidad} elementos`);
+  };
+  let stock = 10;
+  let initial = 1;
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<ItemListContainer />} />
 
-export default App
+          <Route path="/cart" element={<cart />} />
+
+          <Route path="/login" element={<login />} />
+
+          <Route path="*" element={<h1> error 404: Not Found</h1>} />
+        
+        </Routes>
+
+        <ItemCount stock={stock} initial={initial} onAdd={onAdd} />
+      </BrowserRouter>
+    </div>
+  );
+}
