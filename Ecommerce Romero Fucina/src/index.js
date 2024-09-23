5182847e9046014fed2b188dd744c6dfec6d0d6d
jonathan romero fucina <jonathan07_1@hotmail.com>
2024-09-22 23:22:31 -0300

    nuevo repositorio

---
+++ Ecommerce Romero Fucina/src/index.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCscJmXLGLZFEyrDxb4sM29p45YLUMB3D0",
+  authDomain: "my-ecommerce-romero-fucina.firebaseapp.com",
+  projectId: "my-ecommerce-romero-fucina",
+  storageBucket: "my-ecommerce-romero-fucina.appspot.com",
+  messagingSenderId: "58310856787",
+  appId: "1:58310856787:web:3ddd7f2930e9cd1dc236a6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
